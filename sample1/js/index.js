af2af3c9822b473b49177435731b7670c3363cf8
kenjimorita <morita_kenji_gn@cyberagent.co.jp>
2017-03-08 17:47:21 +0900

    init

--- sample1/js/index.js
+++ sample1/js/index.js
@@ -1,1 +1,25 @@
+function setup(){
+    createCanvas(400, 400);
+    background("#fff");
+}
 
+function draw(){
+    drawSprites();
+}
+
+// function touchStarted(){
+//     // ellipse(mouseX, mouseY, 5, 5);
+//     createSprite(mouseX, mouseY, 50, 50);
+// }
+
+function touchMoved(){
+    strokeWeight(10);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    stroke(126);
+}
+
+function touchEnded(){
+    ellipse(mouseX, mouseY, 5, 5);
+    console.log("fafa");
+    return false;
+}
